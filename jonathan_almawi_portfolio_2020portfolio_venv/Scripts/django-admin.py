2d27c4900f3070d2c5de8ffdb817f83460441cb6
CyborgVillager <almawijonathan@gmail.com>
2020-01-13 09:53:18 -0600

    v1 Updated Django Info
    Updated Django Info: Re-edit the files, new info has been inserted

--- jonathan_almawi_portfolio_2020portfolio_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\PyProject\GitRespo\Django_Portfolio\jonathan_almawi_portfolio_2020portfolio_venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
